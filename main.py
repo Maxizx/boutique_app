db5641c2f566558b5fdafc79ba5cb5484a75c44b
maxizx <Pat1t00>
2024-02-02 20:59:14 -0300

    add button cargar in db

--- main.py
+++ main.py
@@ -22,14 +22,8 @@ class App(customtkinter.CTk):
         ventana2 = tkinter.Toplevel()
         ventana2.title("Segunda ventana")
         ventana2.geometry("350x500")
-
-
-        # self.nameLabel = customtkinter.CTkLabel(ventana2,text="Descripcion").grid(row=0, column=0,padx=20, pady=20,sticky="ew")
-        # self.nameEntry = customtkinter.CTkEntry(ventana2,placeholder_text="descripcion").grid(row=0, column=1,columnspan=columnspan, padx=padx,pady=pady, sticky=sticky)
-        # self.variable = self.nameEntry.get()
-
         
-        self.valor = self.tipos_de_vestidos(ventana2,row=0, column=0,padx=20, pady=20,sticky="ew",columnspan=3,texto="Tipo")
+        self.tipo = self.tipos_de_vestidos(ventana2,row=0, column=0,padx=20, pady=20,sticky="ew",columnspan=3,texto="Tipo")
         self.descripcion = self.campo_formulario(ventana2,texto="Descripcion",row=1,column=0,columnspan=3,sticky="ew")
         self.precio_alquiler = self.campo_formulario(ventana2,texto="Precio de Alquiler",row=2,column=0,columnspan=3,sticky="ew")
         self.precio_venta = self.campo_formulario(ventana2,texto="Precio de Venta",row=3,column=0,columnspan=3,sticky="ew")
@@ -39,28 +33,33 @@ class App(customtkinter.CTk):
         self.btn.grid(row=5, pady=20, columnspan=3, sticky="nsew")
 
     def cargar(self):
-        print(f"{self.valor} + {self.descripcion.get()} + {self.precio_alquiler.get()} + {self.precio_venta.get()} + {self.tallas_}")
-
+        print(f"{self.tipo.get()} + {self.descripcion.get()} + {self.precio_alquiler.get()} + {self.precio_venta.get()} + {self.tallas_.get()}")
+        self.bd_prodcutos = productos(True,fecha_de_devolucion=0,imagen="nada",tipos_de_vestidos=self.tipo.get(),descripcion=self.descripcion.get(),talla=self.tallas_.get(),precio_de_venta=int(self.precio_venta.get()),precio_del_alquiler=int(self.precio_alquiler.get()))
+        self.bd_prodcutos.agregar_productos()
 
     def campo_formulario(self,ventana,row:int,column:int,columnspan:int,padx=20,pady=20,sticky = "ew",texto = ""):
         self.nameLabel = customtkinter.CTkLabel(ventana,text=texto)
         self.nameLabel.grid(row=row, column=column,padx=padx, pady=pady,sticky=sticky)
         self.nameEntry = customtkinter.CTkEntry(ventana,placeholder_text=texto)
         self.nameEntry.grid(row=row, column=1+column,columnspan=columnspan, padx=padx,pady=pady, sticky="nsew")
-        # self.variable = self.nameEntry.get()
         return self.nameEntry
+
 
     def tipos_de_vestidos(self,ventana,row:int,column:int,columnspan:int,padx=20,pady=20,sticky = "ew",texto = ""):
         self.nameLabel = customtkinter.CTkLabel(ventana,text=texto)
         self.nameLabel.grid(row=row, column=column,padx=padx, pady=pady,sticky=sticky)
-        self.nameCombo = customtkinter.CTkComboBox(ventana,values=["15 años","damas","novia"]).grid(row=row, column=1+column,columnspan=columnspan, padx=padx,pady=pady, sticky=sticky)
+        self.nameCombo = customtkinter.CTkComboBox(ventana,values=["15 años","damas","novia"])
+        self.nameCombo.grid(row=row, column=1+column,columnspan=columnspan, padx=padx,pady=pady)
+        return self.nameCombo
         
 
 
     def tallas(self,ventana,row:int,column:int,columnspan:int,padx=20,pady=20,sticky = "ew",texto = ""):
         self.nameLabel = customtkinter.CTkLabel(ventana,text=texto)
         self.nameLabel.grid(row=row, column=column,padx=padx, pady=pady,sticky=sticky)
-        self.nameCombo = customtkinter.CTkComboBox(ventana,values=["S","M","L","X","XL","XXL"]).grid(row=row, column=1+column,columnspan=columnspan, padx=padx,pady=pady, sticky=sticky)
+        self.nameCombo = customtkinter.CTkComboBox(ventana,values=["S","M","L","X","XL","XXL"])
+        self.nameCombo.grid(row=row, column=1+column,columnspan=columnspan, padx=padx,pady=pady)
+        return self.nameCombo
 
 
 if __name__ == "__main__":
